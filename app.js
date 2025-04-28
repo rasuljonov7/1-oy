070ef208579722d78ae65ba0a4888eadd649725f
rasuljonov7 <rasuljonov7>
2025-04-28 21:00:31 +0500

    initial commit

--- app.js
+++ app.js
@@ -122,4 +122,115 @@
 
 // showVowelsOnly(harflar)
 
+// ==============================================
+// 9-masala:Maxfiy kodlar
+// ==============================================
 
+
+// function printIndex() {
+//     const letter = ["S", "A", "L", "O", "M"];
+
+//     for (const key in letter) {
+//         console.log(letter[key])
+//     }
+// }
+
+// printIndex()
+
+
+
+
+
+
+// ==============================================
+// 10-masala:Yoshlarni raqamga aylantiring
+// ==============================================
+
+// function convertAgestoSrt() {
+//     const ages = ["11", "45", "73", "12", "20"];
+    
+//     for (const age of ages) {
+//         const num = parseInt(age);
+//         console.log(num);
+//     }
+// }
+
+// convertAgestoSrt()
+
+
+
+
+// ==============================================
+// 11-masala:Kim onlayn
+// ==============================================
+
+
+// function showPlayerStatus() {
+//     const status = [true, false, true, false];
+
+// for (const arr in status) {
+//     if(status[arr] === true){
+//         console.log("Online");
+//     }else{
+//         console.log("Oflayn");
+        
+//     }
+// }
+// }
+
+// showPlayerStatus()
+
+
+
+
+
+// ==============================================
+// 12-masala:Ichimliklar ta'rifi
+// ==============================================
+
+// function describeDrinks() {
+// const drinks = [
+//     ["Cola", "sweet"],
+//     ["Water", "clear"],
+//     ["Juice", "sweet"]
+// ]
+
+// const [[C, s], [W, c], [J, S]] = drinks;
+// console.log(${C} - ${s});
+// console.log(${W} - ${c});
+// console.log(${J} - ${S});
+// }
+
+// describeDrinks()
+
+
+
+// ==============================================
+// 13-masala:Satrlar uzunligini o'lchaymiz
+// ==============================================
+
+
+// function printNamesLength() {
+// const name1 = ["Anora"];
+// const name2 = ["Oyjamol"];
+// const name3 = ["Muhammad"];
+
+
+
+
+// for (const name of name1) {
+//     console.log(${name} - ${name.length});
+// }
+
+
+// for (const name of name2) {
+//     console.log(${name} - ${name.length});
+// }
+
+
+// for (const name of name3) {
+//     console.log(${name} - ${name.length});
+// }
+// }
+
+// printNamesLength()
